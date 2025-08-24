18cb58b9a08e878b410fd5e0c095c4eba4fd8c66
krish mamtora <krishmamtora26@gmail.com>
2025-08-24 17:31:06 +0530

    added basic login page form alog with its css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
           <Link to="/">Home</Link>
         </li>
         <li>
           <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume Uploader</Link>
         </li>
       </ul>
     </nav>
